4a182df5a85e111bbb9a02d9e0772410967e5b51
MarceloLiGonzales <marcelo.ligonzales@ucalgary.ca>
2021-04-04 17:39:53 -0600

    SPI stuff
    Have the TTGO update its time from the PIC

--- platform_IO_MARS/src/main.cpp
+++ platform_IO_MARS/src/main.cpp
@@ -11,6 +11,24 @@
 #define BUTTON_1_PIN        35
 #define BUTTON_2_PIN        0
 
+void getTimefromPIC(void);
+
+SPIClass PICSPI;
+SPISettings PICSPISettings =  {
+                              5000000,
+                              MSBFIRST,
+                              SPI_MODE0
+                              };
+
+uint8_t Time0 = 77;
+uint8_t Time1 = 32;
+uint8_t Time2 = 64;
+
+#define MOSI 12
+#define MISO 13
+#define SPICLK 15
+#define SPICS 17
+
 TFT_eSPI tft_display = TFT_eSPI(135, 240);  //creating display object for usage
 Button2 btn1(BUTTON_1_PIN);                 //creating button object for usage
 Button2 btn2(BUTTON_2_PIN);                 //creating button object for usage
@@ -22,15 +40,29 @@ void setup()
   {
     //record error message
   }
+  PICSPI.begin(SPICLK, MISO, MOSI, SPICS);
 }
 
 void loop()
 {
   delay(10000);
-
+  getTimefromPIC();
 }
 
 //Need websever update task
 
 //need alarm interrupt
 //need button interrupts for features
+
+
+void getTimefromPIC(void)
+{
+  PICSPI.beginTransaction(PICSPISettings);
+  Time0 = PICSPI.transfer(Time0);
+  Serial.print(Time0);
+  Time1 = PICSPI.transfer(Time1);
+  Serial.print(Time1);
+  Time2 = PICSPI.transfer(Time2);
+  Serial.print(Time2);
+  PICSPI.endTransaction();
+}
