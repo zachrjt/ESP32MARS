170cc4ff161fb56510370acb93a4eba66dd8a05e
MarceloLiGonzales <marcelo.ligonzales@ucalgary.ca>
2021-04-06 13:29:17 -0600

    Set up Buttons and SPI
    Testing for SPI and buttons. The GetTimeFromPIC dont quite work, rest seems fine

--- platform_IO_MARS/src/main.cpp
+++ platform_IO_MARS/src/main.cpp
@@ -2,67 +2,167 @@
 #include <TFT_eSPI.h>     //TFT display libary
 #include <SPI.h>          //SPI Libary
 #include <Button2.h>      //Button libary
+#include <SD.h>           //SD card library
 
 #include "task_macros.h"            //task marcos for priority, cores, and stack sizes
 #include "function_macros.h"        //function macros and general typedefs used for development 
 
 #include "peripheral_initialize.h"  //other functions header file
 
-#define BUTTON_1_PIN        35
-#define BUTTON_2_PIN        0
+#define BUTTON_1_PIN        36
+#define BUTTON_2_PIN        37
 
-void getTimefromPIC(void);
+#define SD_MOSI 26
+#define SD_MISO 32
+#define SD_SPICLK 25
+#define SD_SPICS 33
 
-SPIClass PICSPI;
+#define PIC_MOSI 12
+#define PIC_MISO 13
+#define PIC_SPICLK 15
+#define PIC1_SPICS 17
+#define PIC2_SPICS 22
+
+void sendTimetoPIC1(void);        //Send time to PIC
+void getTimefromPIC1(void);      //Get time from PIC
+void sendAlarmFlagtoPIC2(void);   //If Alarm then alarm
+
+void pressed(Button2& btn);       //Button Pressed event
+
+SPIClass SDSPI(HSPI); //defines the spi bus for use with the SD card
+SPIClass PIC1_SPI;    //defines the spi bus for use with the PIC with clock display
+SPIClass PIC2_SPI;    //defines the spi bus for use with the PIC with alarm capabilities
+
 SPISettings PICSPISettings =  {
-                              5000000,
+                              500000, 
                               MSBFIRST,
                               SPI_MODE0
                               };
-
-uint8_t Time0 = 77;
-uint8_t Time1 = 32;
-uint8_t Time2 = 64;
-
-#define MOSI 12
-#define MISO 13
-#define SPICLK 15
-#define SPICS 17
 
 TFT_eSPI tft_display = TFT_eSPI(135, 240);  //creating display object for usage
 Button2 btn1(BUTTON_1_PIN);                 //creating button object for usage
 Button2 btn2(BUTTON_2_PIN);                 //creating button object for usage
 
+uint8_t Time0 = 76;     //Times in PIC display format
+uint8_t Time1 = 32;
+uint8_t Time2 = 64;
+
+uint8_t Time0R = 76;     //Used for debugging GetTimeFromPIC
+uint8_t Time1R = 32;
+uint8_t Time2R = 64;
+
+uint8_t AlarmFlag = 1;
 
 void setup()
 {
+  Serial.begin(115200);
+
   if (!setup_functions())
   {
     //record error message
   }
-  PICSPI.begin(SPICLK, MISO, MOSI, SPICS);
+
+  pinMode(PIC1_SPICS, OUTPUT);
+  digitalWrite(PIC1_SPICS, HIGH);
+  pinMode(PIC2_SPICS, OUTPUT);
+  digitalWrite(PIC2_SPICS, HIGH);
+  PIC1_SPI.begin(PIC_SPICLK, PIC_MISO, PIC_MOSI, PIC1_SPICS);
+  PIC2_SPI.begin(PIC_SPICLK, PIC_MISO, PIC_MOSI, PIC2_SPICS);
+
+
+  pinMode(BUTTON_1_PIN, INPUT);
+  pinMode(BUTTON_2_PIN, INPUT);
+  btn1.setPressedHandler(pressed);
+  btn2.setPressedHandler(pressed);
 }
 
 void loop()
 {
-  delay(10000);
-  getTimefromPIC();
+  btn1.loop();
+  btn2.loop();
+  //getTimefromPIC1();
+  sendTimetoPIC1();
+  sendAlarmFlagtoPIC2();
+}
+
+void sendTimetoPIC1(void)
+{
+  digitalWrite(PIC1_SPICS, LOW);
+  PIC1_SPI.beginTransaction(PICSPISettings);
+
+  PIC1_SPI.transfer(Time0);
+  PIC1_SPI.transfer(Time1);
+  PIC1_SPI.transfer(Time2);
+
+  PIC1_SPI.endTransaction();
+  digitalWrite(PIC1_SPICS, HIGH);
+}
+
+void  getTimefromPIC1(void)
+{
+  digitalWrite(PIC1_SPICS, LOW);
+  PIC1_SPI.beginTransaction(PICSPISettings);
+
+  PIC1_SPI.transfer(0);
+
+  //delay(1000);
+
+  Time0R = PIC1_SPI.transfer(0);
+  Time1R = PIC1_SPI.transfer(0);
+  Time2R = PIC1_SPI.transfer(0);
+
+  Serial.println(Time0R);
+  Serial.println(Time1R);
+  Serial.println(Time2R);
+
+  PIC1_SPI.endTransaction();
+  digitalWrite(PIC1_SPICS, HIGH);
+}
+
+void sendAlarmFlagtoPIC2(void)
+{
+  digitalWrite(PIC2_SPICS, LOW);
+  PIC1_SPI.beginTransaction(PICSPISettings);
+
+  PIC1_SPI.transfer(AlarmFlag);
+
+  PIC1_SPI.endTransaction();
+  digitalWrite(PIC2_SPICS, HIGH);
+}
+
+
+//Button Interrupts (?) for features, for some reason these are each triggered once during Start up so AlarmFlag is default off
+void pressed(Button2& btn) {
+
+  if(btn ==  btn1)
+  {
+    Serial.println("Snooze if Alarm flag is on, do nothing otherwise");
+    if (AlarmFlag)
+    {
+      AlarmFlag = 0;
+
+      //Set up new Alarm event 5 minutes from now here
+      sendAlarmFlagtoPIC2();
+      delay(1000);
+      AlarmFlag = 1; 
+    }
+    else      ///This is just for debugging
+    {
+      //AlarmFlag = 1;
+    }
+  }
+  else if (btn ==  btn2)
+  {
+    Serial.println("Turn off Alarm if its on");
+    if (AlarmFlag)
+    {
+      AlarmFlag = 0;
+
+      //Update event(?) here (not sure if needed)
+    }
+  }
 }
 
 //Need websever update task
 
 //need alarm interrupt
-//need button interrupts for features
-
-
-void getTimefromPIC(void)
-{
-  PICSPI.beginTransaction(PICSPISettings);
-  Time0 = PICSPI.transfer(Time0);
-  Serial.print(Time0);
-  Time1 = PICSPI.transfer(Time1);
-  Serial.print(Time1);
-  Time2 = PICSPI.transfer(Time2);
-  Serial.print(Time2);
-  PICSPI.endTransaction();
-}
